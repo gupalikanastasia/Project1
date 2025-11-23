3603376d78c4ddf3a718db5dcd050313fe417cd6
gupalikanastasia <gupalikanastasia@gmail.com>
2025-11-23 17:25:39 +0200

    Done

---
+++ lesson1.py
@@ -0,0 +1,22 @@
+name = 'Anastasia'
+age = 16
+mark = 10.9
+group = '22ist'
+flaws = []
+colors = ['white', 'blue', 'black']
+student = True
+hobbies = ('books', 'music', 'films')
+grades = {10, 11, 12}
+pets = {'1': 'cat', '2': 'hamster'}
+print(name, ':', type(name))
+print(age, ':', type(age))
+print(mark, ':', type(mark))
+print(group, ':', type(group))
+print(flaws, ':', type(flaws))
+print(colors, ':', type(colors))
+print(student, ':', type(student))
+print(hobbies, ':', type(hobbies))
+print(grades, ':', type(grades))
+print(pets, ':', type(pets))
+
+#коміт
